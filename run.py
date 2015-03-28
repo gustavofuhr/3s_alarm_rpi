6b0172c7cbc4f482a451747cbe8ff7e9823adca0
Gustavo Führ <gustavofuhr87@gmail.com>
2015-03-28 20:00:49 -0300

    Add servo controller using servoblaster to wakeup the guy with water

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 from three_s_alarm_rpi import three_s_alarm_rpi
 
-
 if __name__ == "__main__":
     alarm_clock = three_s_alarm_rpi.ThreeSAlarmRpi()
     alarm_clock.activate()
--- three_s_alarm_rpi/rpi_controller.py
+++ three_s_alarm_rpi/rpi_controller.py
@@ -4,10 +4,13 @@ from pygame import mixer
 
 class RPiController:
 
-    def __init__(self, led_gpio, servo_gpio):
+    def __init__(self, led_gpio, servo_gpio, init_servo_value):
         self.led_gpio = led_gpio
         self.servo_gpio = servo_gpio
         self.clean_up_chanels()
+
+        if init_servo_value is not None:
+            self.move_servo(init_servo_value)
 
     def clean_up_chanels(self):
         # clean up channel
@@ -17,21 +20,27 @@ class RPiController:
         GPIO.setup(channel, GPIO.OUT)
         GPIO.cleanup()
 
-        channel = self.gpio_servo
+        channel = self.servo_gpio
 
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(channel, GPIO.OUT)
         GPIO.cleanup()
 
+    def move_servo(self, position):
+        servoStr = "%u=%u\n" % (self.servo_gpio, position)
+        with open("/dev/servoblaster", "wb") as f:
+            f.write(servoStr)
+
     def turn_on_LED(self, how_long=60 * 60 * 4):
-        channel = self.gpio_led
+        channel = self.led_gpio
 
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(channel, GPIO.OUT)
         GPIO.output(channel, GPIO.HIGH)
 
-    def turn_some_water(self):
-        pass
+    def turn_some_water(self, init_value, end_value):
+        self.move_servo(init_value)
+        self.move_servo(end_value)
         # channel = self.servo_gpio
 
         # GPIO.setmode(GPIO.BCM)
